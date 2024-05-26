767ac5abe028764c683e9d58f0e4e0ad0473db3a
pizza <yulu770520@gmail.com>
2024-05-26 02:12:38 -0400

    recent update (player, mob spawn, infBackground)

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,24 +1,37 @@
 extends CharacterBody2D
 
-var speed = 144.0
+#Stats
+@export var health = 100
+@export var speed = 400
+# Weapon
+@export var BulletScene : PackedScene
+var bulletOffset = 50
 
-func _physics_process(delta): # once every physics frame
-	if Input.is_action_pressed("down"):
-		print("down")
-	if Input.is_action_pressed("up"):
-		print("up")
-	if Input.is_action_pressed("left"):
-		print("left")
-	if Input.is_action_pressed("right"):
-		print("right")
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction.normalized() * speed
+	if velocity == Vector2.ZERO:
+		pass
 	else:
-		print("idle")
-	if Input.is_action_just_pressed("attack"):
-		print("attacked")
-		$AttackArea/wepHitbox.disabled == false
-	movement()
+		$AnimationTree.set("parameters/blend_position", velocity)
+	if Input.is_action_just_pressed("shoot"):
+		var pos = get_global_mouse_position()
+		shoot(pos)
+		#shoot()
 
-func movement():
-	velocity = Input.get_vector("left","right","up","down").normalized() * speed
+func shoot(position_tar):
+	var bullet = BulletScene.instantiate()
+	owner.add_child(bullet)
+	
+	var startingPosition = position + (position_tar - position).normalized() * bulletOffset
+	
+	bullet.position = startingPosition
+	bullet.direction = (position_tar - position).normalized()
+	bullet.rotate_bullet()
+
+func _physics_process(delta):
+	get_input()
 	move_and_slide()
 	
+# creates bullet, makes bullet travel in direction of cursor, removes bullet after traveling some distance, if bullet touches monster enemy
